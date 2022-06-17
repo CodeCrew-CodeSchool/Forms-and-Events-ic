d4c0e7310a58041dd4c45fd23279431e3bc9e25b
Robert Armstrong <robert@code-crew.org>
2022-06-17 10:53:49 -0500

    demo

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 // I want to print the values of my form to the document
     // 1. I will need a reference to my form;
 
-        const form = document.getElementById('myForm');
-        const password = document.getElementById('password');
-        const email = document.getElementById('email')
-
+        
     // 2. I will need to listen to the events on the form
-        form.addEventListener('submit',(e)=>{
-          e.preventDefault()
-          form.reset()
+      
            
-        })
+        
